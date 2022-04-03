4bf6d9ec4309f0fff7fdffd3b92dec4480c7b0f9
emircanisanc <emircanisanc@gmail.com>
2022-04-03 10:56:00 +0300

    Defining and Using a Layout Master page

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.fronbase')
+
+@section('title', 'Title from sub file')
+
+@section('sidebar')
+@parent
+
+<p>This is appended from sub file.</p>
+@endsection
+
+@section('content')
+<p>This is my body content.</p>
+@endsection
